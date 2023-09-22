c8306486fc7c446de6183326955dc3a4ccc623be
MrMaksutka2028 <maksimbutakov59@gmail.com>
2023-09-22 21:21:08 +0500

    forth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import "./App.css";
 import List from "./List.js";
 import Form from "./Form.js";
+import { useState } from "react";
 
-let list = [
-  {id: 1, title: "Завершить проект на Vue", done: true},
-  {id: 2, title: "Завершить проект на React", done: false}
-]
+  function add(title, setState, state) {
+    state.push({
+      id: state[state.length - 1].id + 1,
+      title: title,
+      done: false
+    });
+  };
 
-function add(title) {
-  console.log(title);
-  list.push({
-    id: list[list.length - 1].id + 1,
-    title: title,
-    done: false
-  })
-}
+  function done(id, setState, state) {
+    setState(state.map((task) => {
+      if(id = task.id) {
+        task.done = !task.done
+      }
+      return task;
+    }));
+  };
 
-function App() {
+  function remove(id, setState, state) {
+    setState(state.filter((task) => id != task.id))
+  }
+
+  function App() {
+    const [state, setState] = useState([
+        {id: 1, title: "Завершить проект на Vue", done: true},
+        {id: 2, title: "Завершить проект на React", done: false}
+    ]); 
+
   return (
     <div>
-    <Form onAdd={add} />
-    <List taskList={list} />
+    <Form onAdd={(title) => add(title, setState, state)} />
+    <List onRemove={(id) => remove(id, setState, state)} onDone={(id) => done(id, setState, state)} taskList={state} />
 
     </div>
   );
--- src/List.js
+++ src/List.js
@@ -1,7 +1,10 @@
 export default function List(props) {
     return (
         <ul>
-            {props.taskList.map((task) => <li class={(task.done) ? "done" : ""}>{task.title}</li>)}
+            {props.taskList.map((task) => 
+            <li key={task.id} class={(task.done) ? "done" : ""}>
+            <span> onClick={() => props.onDone(task.id)} {task.title}</span>
+            <button onClick={() => props.onRemove(task.id)}>Удалить</button> </li>)}
         </ul>
     )
 }
